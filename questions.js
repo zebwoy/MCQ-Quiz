251e0f6263e29c57e0e1a63dbc0aa4ebd098d542
zebwoy <75931281+zebwoy@users.noreply.github.com>
2023-02-14 14:32:52 +0530

    pushing files from origin to remote

---
+++ questions.js
@@ -0,0 +1,110 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+  numb: 1, question: "What are the end products of cellular respiration?",
+  answer: "Water and carbon dioxide",
+  options: [
+    "Water and carbon dioxide",
+    "Carbon dioxide and oxygen",
+    "Water and oxygen",
+    "Sugar and oxygen"]
+},
+{
+  numb: 2,
+  question: "The stomata are present on which part of the plant?",
+  answer: "Leaves",
+  options: [
+    "Roots",
+    "Leaves",
+    "Stem",
+    "Flowers"
+  ]
+},
+{
+  numb: 3,
+  question: "Which of the following is not a type of symbiotic relationship?",
+  answer: "Predation",
+  options: [
+    "Mutualism",
+    "Commensalism",
+    "Parasitism",
+    "Predation"
+  ]
+},
+{
+  numb: 4,
+  question: "What are the components of blood?",
+  answer: "Red blood cells, white blood cells, and plasma",
+  options: [
+    "Red blood cells, white blood cells, and plasma",
+    "Platelets, plasma, and hemoglobin",
+    "White blood cells, plasma, and hemoglobin",
+    "Red blood cells, platelets, and plasma"
+  ]
+},
+{
+  numb: 5,
+  question: "The process of photosynthesis takes place in which part of the plant cell?",
+  answer: "Chloroplast",
+  options: [
+    "Vacuole",
+    "Chloroplast",
+    "Mitochondria",
+    "Nucleus"
+  ]
+},
+{
+  numb: 6,
+  question: "Who discovered the structure of DNA?",
+  answer: "James Watson and Francis Crick",
+  options: [
+    "Charles Darwin",
+    "Gregor Mendel",
+    "James Watson and Francis Crick",
+    "Robert Hooke"
+  ]
+},
+{
+  numb: 7,
+  question: "What is the main function of the renal system?",
+  answer: "Excretion of waste products",
+  options: [
+    "Excretion of waste products",
+    "Homeostasis",
+    "Hormone regulation",
+    "Blood circulation"
+  ]
+},
+{
+  numb: 8,
+  question: "Which of the following is not a component of the nervous system?",
+  answer: "Muscle",
+  options: [
+    "Brain",
+    "Spinal cord",
+    "Nerves",
+    "Muscle"
+  ]
+},
+{
+  numb: 9,
+  question: "What are the stages of the menstrual cycle?",
+  answer: "Follicular, ovulatory, and luteal",
+  options: [
+    "Follicular, ovulatory, and luteal",
+    "Proestrus, estrus, and diestrus",
+    "Ovarian, uterine, and follicular",
+    "Menstrual, proliferative, and secretory"
+  ]
+},
+{
+  numb: 10,
+  question: "The hormones secreted by the pancreas regulate which of the following body processes?",
+  answer: "All of the above",
+  options: [
+  "Blood pressure",
+  "Blood sugar levels",
+  "Metabolism",
+  "All of the above"
+]
+},
+];
